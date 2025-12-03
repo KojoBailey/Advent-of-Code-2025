2ecbc51dd14cd8abbf9cfbd1387fb704862907b7
Kojo Bailey <kojomolojo@gmail.com>
2025-12-03 14:39:06 +0000

    remove redundant functions

--- Day 1/Day1.hs
+++ Day 1/Day1.hs
@@ -1,44 +1,17 @@
 -- Day 1: Secret Entrance
 -- https://adventofcode.com/2025/day/1
 
-import Prelude
+-- 0x434C49434B = "CLICK"
 
--- Part 1
+import Prelude
 
 calculatePassword :: String -> IO Int
 calculatePassword path = do
   instructions <- loadInstructions path
   pure $ zeroCount instructions
   where
-    zeroCount instructions = length . filter (== 0) $ evaluateInstructions start instructions
+    zeroCount instructions = length . filter (\(x, _) -> x == 0) $ evaluateClickInstructions start instructions
     start = 50
-
-loadInstructions :: String -> IO [(Char, Int)]
-loadInstructions path = do
-  rawInstructions <- readFile path
-  pure $ parsedInstructions rawInstructions
-  where
-    parsedInstructions rawInstructions = map (\str -> (head str, read $ tail str)) $ lines rawInstructions
-
-evaluateInstructions :: Int -> [(Char, Int)] -> [Int]
-evaluateInstructions _ [] = []
-evaluateInstructions x ((dir, n):rest) =
-  newX : evaluateInstructions newX rest
-  where
-    newX = turnDial dir x n
-
-turnDial :: Char -> Int ->  Int -> Int
-turnDial dir x dx = case dir of
-  'L' -> clamp (-)
-  'R' -> clamp (+)
-  where
-    clamp :: (Int -> Int -> Int) -> Int
-    clamp sign = (x `sign` dx) `mod` xMax
-    xMax = 100
-
-
--- Part 2
--- 0x434C49434B = "CLICK"
 
 calculateClickPassword :: String -> IO Int
 calculateClickPassword path = do
@@ -47,6 +20,13 @@ calculateClickPassword path = do
   where
     zeroCount instructions = foldr (\(_, clicks) acc -> acc + clicks) 0 $ evaluateClickInstructions start instructions
     start = 50
+
+loadInstructions :: String -> IO [(Char, Int)]
+loadInstructions path = do
+  rawInstructions <- readFile path
+  pure $ parsedInstructions rawInstructions
+  where
+    parsedInstructions rawInstructions = map (\str -> (head str, read $ tail str)) $ lines rawInstructions
 
 evaluateClickInstructions :: Int -> [(Char, Int)] -> [(Int, Int)]
 evaluateClickInstructions _ [] = []
